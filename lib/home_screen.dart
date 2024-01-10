78a9e728deeee739391ba130b09c9e92d6a6c0bc
admin <urftariq069@gmail.com>
2024-01-10 12:50:49 +0500

    Image Fetch and Insert

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:uuid/uuid.dart';
 
 class DashBoardScreen extends StatefulWidget {
@@ -16,27 +20,25 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
   TextEditingController userEmail = TextEditingController();
   TextEditingController userPassword = TextEditingController();
 
-  // void userAdd()async{
-  //
-  //   Map<String, dynamic> uAdd = {
-  //     "userName" : userName.text,
-  //     "userEmail" : userEmail.text,
-  //     "userAddress" : userAddress.text,
-  //     "userPassword" : userPassword.text
-  //   };
-  //
-  //   await FirebaseFirestore.instance.collection("userData").add(uAdd);
-  //   ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("User Added Successfully")));
-  //
-  // }
 
-  void userAdd()async{
-
+  void userAddWithImage()async{
     String userID = const Uuid().v1();
+
+    // User Image with ID
+    UploadTask uploadTask = FirebaseStorage.instance.ref().child("UserImages/").child(userID).putFile(userProfile!);
+    TaskSnapshot taskSnapshot = await uploadTask;
+    String userImage = await taskSnapshot.ref.getDownloadURL();
+
+    // User Data with ID
+    userAdd(userID: userID, userImage: userImage);
+  }
+
+  void userAdd({String? userID, String? userImage})async{
 
     Map<String, dynamic> uAdd = {
       "userID" : userID,
       "userName" : userName.text,
+      "userImage" : userImage,
       "userEmail" : userEmail.text,
       "userAddress" : userAddress.text,
       "userPassword" : userPassword.text
@@ -53,9 +55,71 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
     userAddress.clear();
   }
 
+  File? userProfile;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Firebase CRUD"),
+        actions: [
+          GestureDetector(
+            onTap: (){
+              showAdaptiveDialog(context: context, builder: (context) {
+                return AlertDialog(
+                  title: const Text("Select Your Image Source?"),
+                  actions: [
+                    TextButton(onPressed: ()async{
+                      XFile? selectImage = await ImagePicker().pickImage(source: ImageSource.camera);
+                      if(selectImage != null){
+                        File convertedFile = File(selectImage.path);
+                        setState(() {
+                          userProfile = convertedFile;
+                        });
+                      }
+                      else{
+                        if(context.mounted){
+                          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Image Not Selected")));
+                        }
+                      }
+                      if(context.mounted){
+                        Navigator.pop(context);
+                      }
+                    }, child: const Text("Select From Camera", style: TextStyle(color: Colors.red),)),
+                    TextButton(onPressed: ()async{
+                      XFile? selectImage = await ImagePicker().pickImage(source: ImageSource.gallery);
+                      if(selectImage != null){
+                        File convertedFile = File(selectImage.path);
+                        setState(() {
+                          userProfile = convertedFile;
+                        });
+                      }
+                      else{
+                        if(context.mounted){
+                          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Image Not Selected")));
+                        }
+                      }
+                      if(context.mounted){
+                        Navigator.pop(context);
+                      }
+                    }, child: const Text("Select From Gallery")),
+                  ],
+                );
+              },);
+            },
+            child: userProfile == null ?
+            const CircleAvatar(
+              backgroundImage: NetworkImage('https://cdn3d.iconscout.com/3d/premium/thumb/man-9251877-7590869.png?f=webp'),
+              backgroundColor: Colors.blue,
+            ): CircleAvatar(
+              backgroundImage: userProfile !=null ? FileImage(userProfile!) : null,
+              backgroundColor: Colors.blue,
+            ),
+          ),
+          const SizedBox(
+            width: 14,
+          ),
+        ],
+      ),
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(crossAxisAlignment: CrossAxisAlignment.start,
@@ -94,7 +158,7 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
             ),
 
             Center(child: ElevatedButton(onPressed: (){
-              userAdd();
+              userAddWithImage();
             }, child: const Text("Add User"))),
 
 
@@ -119,33 +183,116 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
                       shrinkWrap: true,
                       itemBuilder:  (context, index) {
 
-                        String userName = snapshot.data!.docs[index]["userName"];
+                        String uName = snapshot.data!.docs[index]["userName"];
+                        String uEmail = snapshot.data!.docs[index]["userEmail"];
+                        String uPassword = snapshot.data!.docs[index]["userPassword"];
+                        String uAddress = snapshot.data!.docs[index]["userAddress"];
+                        String uImage = snapshot.data!.docs[index]["userImage"];
                         String uID = snapshot.data!.docs[index]["userID"];
                         //String userEmail = snapshot.data!.docs[index]["userEmail"];
 
                       return ListTile(
-                        title: Text(userName),
-                        subtitle: Text("${snapshot.data!.docs[index]["userEmail"]}"),
-                        trailing: IconButton(onPressed: (){
-                         showAdaptiveDialog(
-                           context: context,
-                           builder: (context) {
-                           return  AlertDialog(
-                             title: const Text("Do You want to Delete?"),
-                             actions: [
-                               IconButton(onPressed: ()async{
-                                 await FirebaseFirestore.instance.collection("userData").doc(uID).delete();
-                                 if(context.mounted){
-                                   Navigator.pop(context);
-                                 }
-                               }, icon: const Text("Delete", style: TextStyle(color: Colors.red),)),
-                               IconButton(onPressed: (){
-                                 Navigator.pop(context);
-                               }, icon: const Text("Cancel")),
-                             ],
-                           );
-                         },);
-                        }, icon: const Icon(Icons.delete,color: Colors.red,)),
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage(uImage),
+                        ),
+                        title: Text(uName),
+                        subtitle: Text(uEmail),
+                        trailing: SizedBox(
+                          width: 100,
+                          child: Row(
+                            children: [
+                              IconButton(onPressed: (){
+                                showAdaptiveDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return  AlertDialog(
+                                      title: const Text("Do You want to Delete?"),
+                                      actions: [
+                                        TextButton(onPressed: ()async{
+                                          await FirebaseFirestore.instance.collection("userData").doc(uID).delete();
+                                          if(context.mounted){
+                                            Navigator.pop(context);
+                                          }
+                                        }, child: const Text("Delete", style: TextStyle(color: Colors.red),)),
+                                        TextButton(onPressed: (){
+                                          Navigator.pop(context);
+                                        }, child: const Text("Cancel")),
+                                      ],
+                                    );
+                                  },);
+                              }, icon: const Icon(Icons.delete,color: Colors.red,)),
+                              IconButton(onPressed: (){
+                                showBottomSheet(
+                                  context: context,
+                                  builder: (context) {
+                                    TextEditingController uUserName = TextEditingController();
+                                    TextEditingController uUserAddress = TextEditingController();
+                                    TextEditingController uUserEmail = TextEditingController();
+                                    TextEditingController uUserPassword = TextEditingController();
+
+                                    uUserName.text = uName;
+                                    uUserAddress.text = uAddress;
+                                    uUserEmail.text = uEmail;
+                                    uUserPassword.text = uPassword;
+                                  return Container(
+                                    margin: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                                    height: 500,
+                                    child: Column(
+                                      children: [
+                                      const SizedBox(
+                                      height: 20,
+                                    ),
+
+                                        const Text("Update User"),
+                                        const SizedBox(
+                                          height: 20,
+                                        ),
+                                      TextFormField(
+                                      decoration: const InputDecoration(
+                                      hintText: "Enter Your Name"
+                                      ),
+                                      controller: uUserName,
+                                      ),
+                                      TextFormField(
+                                      decoration: const InputDecoration(
+                                      hintText: "Enter Your Address"
+                                      ),
+                                      controller: uUserAddress,
+                                      ),
+                                      TextFormField(
+                                      decoration: const InputDecoration(
+                                      hintText: "Enter Your Email"
+                                      ),
+                                      controller: uUserEmail,
+                                      ),
+                                      TextFormField(
+                                      decoration: const InputDecoration(
+                                      hintText: "Enter Your Password"
+                                      ),
+                                      controller: uUserPassword,
+                                      ),
+
+                                      const SizedBox(
+                                      height: 20,
+                                      ),
+
+                                      Center(child: ElevatedButton(onPressed: ()async{
+                                      await FirebaseFirestore.instance.collection("userData").doc(uID).update(
+                                          {
+                                            "userName" : uUserName.text,
+                                            "userEmail" : uUserEmail.text,
+                                            "userAddress" : uUserAddress.text,
+                                            "userPassword" : uUserPassword.text
+                                          });
+                                      Navigator.pop(context);
+                                      }, child: const Text("Update User"))),]
+                                    ),
+                                  );
+                                },);
+                              }, icon: const Icon(Icons.update,color: Colors.orange,))
+                            ],
+                          ),
+                        ),
                       );
                     },);
                   }
